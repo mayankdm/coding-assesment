ef1846158ca6bbcaffc82a45e1a7d4727e7451fd
mdwivedim <mdwivedim@outlook.com>
2023-01-12 09:00:47 +0000

    new assesment repository

---
+++ src/main/java/java/training/assesments/AssesmentsApplication.java
@@ -0,0 +1,13 @@
+package java.training.assesments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssesmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssesmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/training/assesments/AssesmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package java.training.assesments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
